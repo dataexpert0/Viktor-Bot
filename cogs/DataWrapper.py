0161dc410685cba51cbad5b3c4fed4e1e9583af5
dataexpert0 <gustavom1283@gmail.com>
2025-06-27 17:04:54 -0300

    Adição de novas funcionalidades de data analysis

--- cogs/DataWrapper.py
+++ cogs/DataWrapper.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 import pandas as pd 
 import seaborn as sns 
 import json
+import matplotlib.pyplot as plt
 from datetime import datetime
 
 
@@ -27,10 +28,11 @@ def gerar_novo_id(dados):
     return max([s['id'] for s in dados], default=0) + 1
 
 class ScrimButtons(View):
-    def __init__(self, adversario, mapa):
+    def __init__(self, line, adversario, mapa):
         super().__init__()
         self.adversario = adversario
         self.mapa = mapa
+        self.line = line
 
     @discord.ui.button(label="Vitória", style=discord.ButtonStyle.success)
     async def vitoria_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -48,6 +50,7 @@ class ScrimButtons(View):
             "resultado": resultado,
             "adversario": self.adversario,
             "mapa": self.mapa,
+            "line": self.line,
             "usuario_id": str(interaction.user.id)
         }
         dados.append(nova_scrim)
@@ -60,16 +63,17 @@ class DataWrapper(commands.Cog):
         self.bot = bot
 
     @commands.command(name="scrim")
-    async def scrim_comando(self, ctx, adversario: str, *, mapa: str = "Summoner's Rift"):
+    async def scrim_comando(self, ctx, line: str, adversario: str, *, mapa: str = "Summoner's Rift"):
         embed = discord.Embed(title="Registrar Scrim", color=discord.Color.blurple())
         embed.add_field(name="Adversário", value=adversario)
         embed.add_field(name="Mapa", value=mapa)
+        embed.add_field(name="Line", value=line)
         embed.set_footer(text="Clique em Vitória ou Derrota para registrar")
 
-        await ctx.send(embed=embed, view=ScrimButtons(adversario, mapa))
+        await ctx.send(embed=embed, view=ScrimButtons(line, adversario, mapa))
 
     @commands.command(name="listar_scrims")
-    async def listar_scrims(self, ctx):
+    async def listarscrims(self, ctx):
         dados = dataload()
         if not dados:
             await ctx.send("📭 Nenhuma scrim registrada ainda.")
@@ -78,9 +82,37 @@ class DataWrapper(commands.Cog):
         texto = ""
         for s in dados[-10:]:  # últimos 10
             resultado = "✅" if s["resultado"] == "vitória" else "❌"
-            texto += f"{resultado} {s['adversario']} ({s['mapa']}) por <@{s['usuario_id']}> em {s['data'][:10]}\n"
+            texto += f"{resultado} {s['adversario']} {s['line']} ({s['mapa']}) por <@{s['usuario_id']}> em {s['data'][:10]}\n"
 
         await ctx.send(f"📋 Últimas scrims registradas:\n```{texto}```")
+
+    @commands.command(name = "resultstats")
+    async def resultstats(self, ctx):
+        dados = dataload()
+        if not dados:
+            await ctx.send("Não existe nenhuma scrim registrada ainda.")
+            return
+        
+        df = pd.DataFrame(dados)
+
+        vitorias = 0
+        derrotas = 0
+
+        for resultados in df['resultado']:
+            if resultados == "vitória":
+                vitorias += 1
+            else:
+                derrotas += 1
+
+        plt.pie([vitorias, derrotas], labels = ["Vitórias", "Derrotas"], autopct = "%1.1f%%", colors = ["#66bb6a", "#ef5350"])
+        plt.title("Taxa de Vitória nas Scrims")
+        plt.show()
+
+        imagem_path = "resultados_scrims.png"
+        plt.savefig(imagem_path)
+        plt.close()
+
+        await ctx.send(file=discord.File(imagem_path))
 
 
 async def setup(bot):
